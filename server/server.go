5564f62a0aece4a0c59f1d8d0f159f9d4c887f36
gaochuang <ienjoyarmlinux@163.com>
2023-07-25 21:44:22 +0800

    Add server handler login

--- server/server.go
+++ server/server.go
@@ -29,9 +29,36 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
-func (this *Server) serverHandle(conn net.Conn) {
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
 
+		this.MapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.MapLock.Unlock()
+	}
 }
+
+func (this *Server) BroadCast(user *user.User, msg string) {
+
+	sendMsg := "[" + user.Name + "]" + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+func (this *Server) serverHandle(conn net.Conn) {
+	user := user.NewUser(conn)
+	this.MapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.MapLock.Unlock()
+
+	this.BroadCast(user, "online now")
+
+	select {}
+}
+
 func (this *Server) StartServer() error {
 	server, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.IP, this.port))
 	if err != nil {
@@ -40,6 +67,9 @@ func (this *Server) StartServer() error {
 	}
 
 	defer server.Close()
+
+	//listen message
+	go this.ListenMessage()
 
 	for {
 		conn, err := server.Accept()
